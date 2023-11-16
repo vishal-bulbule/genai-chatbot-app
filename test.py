eaa766cfd5169fab5d13a0285eef9dfd0931e765
vishal.bulbule <vishal.bulbule@techtrapture.com>
2023-11-16 15:18:10 +0530

    Initial code

---
+++ test.py
@@ -0,0 +1,44 @@
+import os
+#import google.cloud.logging
+import vertexai
+
+PROJECT_ID = "tt-dev-001"  # Your Google Cloud Project ID
+LOCATION = "us-central1"  # Your Google Cloud Project Region
+
+#client = google.cloud.logging.Client(project=PROJECT_ID)
+#client.setup_logging()
+
+#LOG_NAME = "non-flask-internal-logs"
+#logger = client.logger(LOG_NAME)
+
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+def create_session():
+    chat_model = vertexai.language_models.ChatModel.from_pretrained("chat-bison@001")
+    chat = chat_model.start_chat()
+    return chat
+
+def response(chat, message):
+    parameters = {
+        "temperature": 0.2,
+        "max_output_tokens": 256,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    result = chat.send_message(message, **parameters)
+    return result.text
+
+def run_chat():
+    chat_model = create_session()
+    print(f"Chat Session created")
+    
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ['exit', 'quit']:
+            break
+        
+        content = response(chat_model, user_input)
+        print(f"AI: {content}")
+
+if __name__ == '__main__':
+    run_chat()
